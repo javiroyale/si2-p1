3bf2ba1721be8458d571bff0553b7d585f461e70
Sandra <ssndra2005@gmail.com>
2026-02-16 12:27:19 +0100

    no se

--- P1-base/visaSite/settings.py
+++ P1-base/visaSite/settings.py
@@ -19,7 +19,7 @@ import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 # Load environment variables
-env_file = BASE_DIR / 'env'
+env_file = BASE_DIR / '.env'
 load_dotenv(dotenv_path=env_file)
 
 # Quick-start development settings - unsuitable for production
