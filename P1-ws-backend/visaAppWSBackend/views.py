95e694ef520bb9aff1c72761db5faf46a91c1b22
Sandra <ssndra2005@gmail.com>
2026-02-18 21:42:30 +0100

    backend hecho

--- P1-ws-backend/visaAppWSBackend/views.py
+++ P1-ws-backend/visaAppWSBackend/views.py
@@ -13,7 +13,7 @@ class TarjetaView(APIView):
         serializer = TarjetaSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            if verificar_tarjeta(serializer.validated_data): 
+            if verificar_tarjeta(serializer.data): 
                 return Response({'message' : 'Datos encontrados en la base de datos'}, status=status.HTTP_200_OK)
         
         return Response({'message': 'Datos no encontrados en la base de datos'}, status=status.HTTP_404_NOT_FOUND)
