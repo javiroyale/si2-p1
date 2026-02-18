c7ba314f4bd6389474babf833a5ddf24fe3084ab
Sandra <ssndra2005@gmail.com>
2026-02-18 21:54:28 +0100

    backend hecho

--- P1-ws-backend/visaAppWSBackend/test_views.py
+++ P1-ws-backend/visaAppWSBackend/test_views.py
@@ -49,6 +49,7 @@ class ApiViewTest(TestCase):
 
     def test_01_tarjeta_check_valid_data(self):
         # Test storing valid data
+        print("data antes de responder", self.tarjeta_data)
         response = self.client.post(
             self.url_tarjeta_check,
             data=self.tarjeta_data,  # tarjeta data
